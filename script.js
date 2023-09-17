4d64b006ddf28bf776f469b85a1761c9c43669a8
GermanAbel <145286557+GermanAbel@users.noreply.github.com>
2023-09-17 13:01:28 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,79 @@
+//MENU 
+const divtoShow = 'nav .menu';
+const divPopup = document.querySelector(divtoShow);
+const divTrigger = document.querySelector('.m-trigger');
+
+divTrigger.addEventListener('click', () => {
+    setTimeout(() => {
+        if(!divPopup.classList.contains('show')){
+            divPopup.classList.add('show');
+            document.body.classList.add('menu-visible');
+        }
+    }, 250);
+})
+//CERRADO AUTOMATICO AL HACER CLICK SOBRE EL MENU
+document.addEventListener('click', (e)=>{
+    const isClosest = e.target.closest(divtoShow);
+    if(!isClosest && divPopup.classList.contains('show')){
+        divPopup.classList.remove('show');
+        document.body.classList.remove('menu-visible');
+    }
+})
+//BUSCAR
+const sTrigger = document.querySelector('.s-trigger');
+const addClass = document.querySelector('.sitio');
+sTrigger.addEventListener('click', () => {
+    addClass.classList.toggle('showsearch');
+})
+
+//SLIDER
+const sliderThumb = new Swiper('.thumb-nav', {
+    spaceBetween: 10,
+    slidesPerView: 3,
+    slidesPerGroup: false,
+    breakpoints: {
+        992: {
+            direction: 'vertical'
+        }
+    }
+});
+const theSlider = new Swiper('.thumb-big', {
+    slidePerView: 1,
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    thumbs: {
+        swiper: sliderThumb,
+    }
+});
+
+
+//GALERIA DE FOTOS (TABBED)
+const tabbeNav = new Swiper('.tnav', {
+    spaceBetween: 8,
+    slidesPerView: 6,
+    centeredSlides: true,
+    slidesPerGroup: false,
+    
+});
+const theTab = new Swiper('.tabbed-item', {
+    loop: true,
+    slidePerView: 1,
+    autoHeight: true,
+    thumbs: {
+        swiper: tabbeNav,
+    }
+});
+/*Transicion al scrolear*/
+const io = new IntersectionObserver(entries =>{
+    entries.forEach(entry =>{
+        if(entry.intersectionRatio > 0){
+            entry.target.classList.add('this');
+        }
+    })
+})
+
+const box = document.querySelectorAll('.animate');
+    box.forEach((el) => {
+        io.observe(el);
+    })
